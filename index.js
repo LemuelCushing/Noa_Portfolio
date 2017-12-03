8ff558773a85d824374df84a8d6e26be00bc48b0
LemuelCushing <SeventhHorror@gmail.com>
2017-12-03 02:41:49 +0200

    too late to curve

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 console.log("x")
 
 let rand = function (min,max) {
+    //The maximum is inclusive and the minimum is inclusive 
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
 
 function randomGray(min,max,h = 0,s = 0 ) {
